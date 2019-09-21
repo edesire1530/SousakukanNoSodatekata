783a092c8fe700b924b93b5cadd1b96a1c33644f
edesire1530 <edesire1530@gmail.com>
2019-09-22 06:19:20 +0900

    test

---
+++ RealityKitDemo/RealityKitDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RealityKitDemo
+//
+//  Created by gree_mini on 2019/09/22.
+//  Copyright © 2019 gree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //test
+    }
+
+
+}
+
